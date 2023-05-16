19ff8931909b532101c0e040aedb75eefab1531e
mrleidesen <mrleidesen@outlook.com>
2023-05-16 08:45:16 +0800

    chore: 替换 windicss 为 taliwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
