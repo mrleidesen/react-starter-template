dfbca60233d8395ae54cd6e0a34ccedb362805e1
mrleidesen <mrleidesen@outlook.com>
2023-09-26 20:24:52 +0800

    feat: add i18next

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,5 +1,20 @@
+import { Trans, useTranslation } from 'react-i18next';
+
 const HomeView: React.FC = () => {
-  return <h1 className="text-blue-500 font-bold text-2xl">Home page</h1>;
+  const { i18n } = useTranslation();
+
+  const handleToggleLanguage = () => {
+    i18n.changeLanguage(i18n.language === 'zh' ? 'en' : 'zh');
+  };
+
+  return (
+    <div>
+      <button onClick={handleToggleLanguage}>Change</button>
+      <h1 className="text-blue-500 font-bold text-2xl">
+        <Trans>title</Trans>
+      </h1>
+    </div>
+  );
 };
 
 export default HomeView;
