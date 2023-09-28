9ebbec1338b7a45f0224b0ec9d63bd51cca5cd3d
mrleidesen <mrleidesen@outlook.com>
2023-09-28 11:18:22 +0800

    fix: 配置 ky 请求

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,31 +4,23 @@ import ky from 'ky';
 
 import { getToken } from '.';
 
-export const request = async <T>(url: Input, options?: Options) => {
-  const urlToString = url.toString();
-  const isURLHasPrefixURL =
-    urlToString.startsWith('http:') || urlToString.startsWith('https:');
-
-  return await ky(url, {
-    // FIXME: 自行设定 prefixUrl
-    prefixUrl: isURLHasPrefixURL
-      ? undefined
-      : 'https://jsonplaceholder.typicode.com',
+const kyOptions = (isBlob?: boolean) => {
+  const options: Options = {
     hooks: {
       beforeRequest: [
         (request) => {
-          // FIXME: 自行处理 headers
           const token = getToken();
+
           if (token) {
             request.headers.set('Authorization', `Bearer ${token}`);
           }
         },
       ],
       beforeError: [
-        // FIXME: 自行处理错误拦截
         (error) => {
           const response = error.response;
 
+          // TODO: 具体接口再修改
           if (!response.ok) {
             console.log(`${response.status}:${response.statusText}`);
           }
@@ -36,7 +28,56 @@ export const request = async <T>(url: Input, options?: Options) => {
           return error;
         },
       ],
+      afterResponse: isBlob
+        ? undefined
+        : [
+            async (_request, _options, response) => {
+              if (!response.ok) {
+                return response;
+              }
+
+              const result = await response.json();
+
+              // FIXME: 根据实际项目修改，有些项目会嵌套
+              // { data: T, status: '200' }
+              // 这种形式返回
+              return new Response(
+                JSON.stringify(result?.data ?? result),
+                response
+              );
+            },
+          ],
     },
+  };
+
+  return options;
+};
+
+const getPrefixUrl = (url: Input) => {
+  const urlToString = url.toString();
+  const isURLHasPrefixURL =
+    urlToString.startsWith('http:') || urlToString.startsWith('https:');
+
+  // FIXME: 替换 api 地址
+  return isURLHasPrefixURL ? undefined : 'https://jsonplaceholder.typicode.com';
+};
+
+export const request = async <T>(url: Input, options?: Options) => {
+  const resp = await ky(url, {
+    prefixUrl: getPrefixUrl(url),
+    ...kyOptions(),
     ...options,
-  }).json<T>();
+  });
+
+  return await resp.json<T>();
+};
+
+export const blobRequest = async (url: Input, options?: Options) => {
+  const resp = await ky(url, {
+    prefixUrl: getPrefixUrl(url),
+    ...kyOptions(true),
+    ...options,
+  });
+
+  return await resp.blob();
 };
